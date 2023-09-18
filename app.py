7fd354c58c61555afaa826b87e5a982a4daa4ffe
Christian Lezcano <clezcanopy@gmail.com>
2023-09-18 02:39:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,41 @@
 import streamlit as st
 import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
-import pandas as pd
-import os
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 # Set up Streamlit
-st.title("Emotion Detection with Fine-Tuned Transformers")
+st.title("Emotion Detection with Transformers")
 
 # Text input
 user_input = st.text_area("Enter your text:")
 
-# Function to load fine-tuned model and tokenizer using @st.cache_data
+
+# Function to load model and tokenizer using @st.cache_data
 @st.cache_data()
 def load_model_and_tokenizer():
-    model_name = "bert-base-uncased"
+    model_name = "mrm8488/t5-base-finetuned-emotion"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    
-    # Load the fine-tuned weights
-    fine_tuned_weights_path = "fine_tuned_emotion_model"
-    if os.path.exists(fine_tuned_weights_path):
-        model.load_state_dict(torch.load(fine_tuned_weights_path))
-    
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     return tokenizer, model
 
+
 tokenizer, model = load_model_and_tokenizer()
+
 
 # Function to analyze emotion
 def analyze_emotion(text):
     if text.strip() == "":
         return "Please enter some text to analyze."
-    
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-    with torch.no_grad():
-        outputs = model(**inputs)
-    
-    logits = outputs.logits
-    predicted_class = torch.argmax(logits, dim=1).item()
-    
-    # Emotion labels
-    emotion_labels = ["anger", "joy", "sadness", "neutral"]
-    emotion = emotion_labels[predicted_class]
-    
-    return f"Emotion: {emotion.capitalize()}"
+
+    input_ids = tokenizer.encode(text + '</s>', return_tensors='pt')
+
+    output = model.generate(input_ids=input_ids,
+                            max_length=2)
+
+    dec = [tokenizer.decode(ids, skip_special_tokens=True) for ids in output]
+    label = dec[0]
+
+    return f"Emotion: {label.capitalize()}"
+
 
 # Analyze button
 if st.button("Analyze Emotion"):
