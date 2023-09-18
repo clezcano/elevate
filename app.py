b5c26189825f1a292679d0c19f36a08d71250f5d
Christian Lezcano <clezcanopy@gmail.com>
2023-09-17 22:23:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 # Set up Streamlit
 st.title("Emotion Detection with Transformers")
@@ -8,29 +8,35 @@ st.title("Emotion Detection with Transformers")
 # Text input
 user_input = st.text_area("Enter your text:")
 
-# Load the model and tokenizer using st.cache_data
+# Function to load model and tokenizer using @st.cache_data
 @st.cache_data()
 def load_model_and_tokenizer():
     model_name = "distilbert-base-uncased"
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    sentiment_analyzer = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-    return sentiment_analyzer
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    return tokenizer, model
 
-sentiment_analyzer = load_model_and_tokenizer()
+tokenizer, model = load_model_and_tokenizer()
 
 # Function to analyze emotion
 def analyze_emotion(text):
     if text.strip() == "":
         return "Please enter some text to analyze."
     
-    result = sentiment_analyzer(text)
-    emotion = result[0]["label"]
-    confidence = result[0]["score"]
-    return f"Emotion: {emotion.capitalize()} (Confidence: {confidence:.2f})"
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    logits = outputs.logits
+    predicted_class = torch.argmax(logits, dim=1).item()
+    
+    # Emotion labels (customize as needed)
+    emotion_labels = ["anger", "joy", "sadness", "neutral"]
+    emotion = emotion_labels[predicted_class]
+    
+    return f"Emotion: {emotion.capitalize()}"
 
 # Analyze button
 if st.button("Analyze Emotion"):
     result = analyze_emotion(user_input)
     st.write(result)
-
